b6858e173ba9d7aca0671e177893f03f2d69429b
solomonyaw <sadeklo@st.vvu.edu.gh>
2017-03-28 21:14:04 +0100

    admin files
    admin model

---
+++ admin/aboutus.php
@@ -0,0 +1,47 @@
+<?php 
+require("header.php");
+//require("checkUser.php")?>
+
+
+<script type="text/javascript">
+	document.getElementById("aaboutus").className="active";
+</script>
+<style>
+.art-PostContent
+{
+  font-family: Georgia, "Times New Roman", Times, Serif;
+  color: #242424;
+  background-color:white;
+ width:898px;
+  padding:5px;
+  font-size:15px;
+}	
+img
+{
+padding-left:200px;
+}
+</style>
+ <div class="art-PostContent">
+ <div class="art-contentLayout">
+   <img src = "res/images/VVUDiscuss.jpg" width=400 height=200/>            
+                            
+<p align="justify">The <b style="color:brown;">Valley View University Information Technology (Discussion) Forum(VVUITF)</b> is an initiative by the Institute of Computer Science to make available the dissemination of relevant information to students of the institute and to increase their understanding of various topics in computer science .The <b style="color:brown;">Valley View University Information Technology Discussion Forum</b> is a discussion forum that gives information about various programming languages, general knowledge related questions, information related to Asp.net,Vb.net ,Java,Php,Os related questions,etc. </p>
+
+<p align="justify">This forum is useful for computer science and information technology students to get information  related to  various topics in computer science.Students have to create an account to be able to login into the forum. Once the student has created the account he becomes the connected user. When some other students asks the  questions these connected students can reply to the questions by giving an answer. The student who logged in can select the questions according to the category. The administrator can insert new topic  dynamically. The connected students while logged in can exchange messages with each other. When the student asks the question related to any topic all the related question  and their answers will be displayed. Students should note that questions and answers that is irrelevant to the forum will be deleted by the Administrator   </p>
+
+ <p>
+                                	<span class="art-button-wrapper">
+                                		<span class="l"> </span>
+                                		<span class="r"> </span>
+                                		<a class="art-button" href="contact.php">Contact Info...</a>
+                                	</span>
+                                </p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<?php require("footer.php")?>
---
+++ admin/logout.php
@@ -0,0 +1,10 @@
+<?php require("header.php");?>
+<?php 
+session_start();
+
+?>
+<h1>log out</h1>
+<p>
+	You have logged out.  <a href="../index.php">Click here</a> to login again.
+</p>
+<?php require("footer.php");?>
